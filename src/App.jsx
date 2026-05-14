7e0267e3cb27f6838658df09e752e54711303b3d
MDVR544 <devriescool@gmail.com>
2026-05-14 13:05:56 +0200

    Opdracht 2 t/m stap 8 klaar

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,33 @@ function App() {
     const [countries, setCountries] = useState([])
     const [error, toggleError] = useState(false);
     const [loading, toggleLoading] = useState(false);
+    const [countryInfo, setCountryInfo] = useState({});
 
     const endPoint = 'https://restcountries.com/v3.1/all?fields=name,flags,population,region,cca3'
 
     async function consoleLogger() {
-        const response = await axios.get(endPoint);
+        const response = await axios.get('https://restcountries.com/v3.1/name/netherlands');
         console.log(response.data)
+        console.log(response.data[1].capital)
+
     }
 
+
     async function fetchCountry(){
+        toggleError(false)
+        toggleLoading(true)
+        try {
+            const response = await axios.get('https://restcountries.com/v3.1/name/netherlands');
+            setCountryInfo(response.data[1])
+        } catch (e) {
+            toggleError(true);
+            console.error(e);
+        } finally {
+            toggleLoading(false);
+        }
+    }
+
+    async function fetchCountries(){
         toggleError(false)
         toggleLoading(true)
         try {
@@ -29,7 +47,6 @@ function App() {
                     return 0;
                 });
             setCountries(sortedCountries);
-
         } catch (e) {
             toggleError(true);
             console.error(e);
@@ -40,13 +57,19 @@ function App() {
 
     return (
         <>
+
             <main>
                 <button onClick={consoleLogger}>Console Log</button>
-                <button disabled={loading} onClick={fetchCountry}>Klik hier! om je land op te halen</button>
+
+                <button disabled={loading} onClick={fetchCountry}>klik hier om info van één land op te halen</button>
+                {countryInfo.name && <p><img src={countryInfo.flag} alt="Flag"/> {countryInfo.name.common}</p>}
+                {countryInfo.name && <p>{countryInfo.name.common} is situated in {countryInfo.subregion} and the capital is {countryInfo.capital}</p>}
+
+                <button disabled={loading} onClick={fetchCountries}>Klik hier! om je land op te halen</button>
                 {countries.map((country) => {
                     return (
                         <article key={country.cca3}>
-                            <img src={country.flag} alt="Vlag"/>
+                            <img src={country.flag} alt="Flag"/>
                             <ColorPicker className={country.region}>{country.name.common}</ColorPicker>
                             <li >has a population of {country.population} people</li>
                         </article>
